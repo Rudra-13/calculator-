9b46df5f1393dbba35cb0365ef1cafa836dfa689
Rudra-13 <rudranshlohomi@gmail.com>
2024-08-12 16:04:09 +0530

    added

---
+++ script.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    if request.method == 'POST':
+        expression = request.form['expression']
+        try:
+            result = str(eval(expression))
+        except Exception as e:
+            result = 'Error'
+
+        return render_template('index.html', result=result)
+calculator
+return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
